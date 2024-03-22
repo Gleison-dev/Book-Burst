08da6109b23732eff60a0e4ea08d34e27982178c
Gleison <gleisonrgomes@gmail.com>
2024-03-22 15:14:27 -0300

    mudança de banco de dados

---
+++ backend/src/database/connection.js
@@ -0,0 +1,20 @@
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: 'path/to/database.sqlite'
+  });
+
+const testConnection = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log("Conexão com o banco de dados realizada com sucesso!");
+    } catch (error) {
+        console.log("Conexão mal sucedida :(", error);
+    }
+}
+
+export { sequelize, testConnection }
