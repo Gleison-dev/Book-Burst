26a99897c5178a0f5421b114393d190d27cdea7a
Gleison <gleisonrgomes@gmail.com>
2024-03-25 20:43:19 -0300

    feat: added logo in header

---
+++ frontend/src/components/Images/Logo.jsx
@@ -0,0 +1,7 @@
+import logo from "../../assets/logo_book_burst.png"
+
+export default function Logo(){
+    return (
+        <img src={logo} alt="logo" />
+    )
+}
---
+++ frontend/src/screens/Home/Home.jsx
@@ -0,0 +1,7 @@
+import Logo from "../../components/Images/Logo";
+
+export default function Home(){
+    return (
+        <Logo/>
+    )
+}
