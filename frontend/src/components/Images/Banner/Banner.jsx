9d588df3149fb5b328b6800b84b57142715ef1fd
Gleison <gleisonrgomes@gmail.com>
2024-03-27 14:07:17 -0300

    added images, logo and banner

---
+++ frontend/src/components/Images/Banner/Banner.jsx
@@ -0,0 +1,8 @@
+import banner from "../../../assets/banner_inicial.png"
+import "./style.css"
+
+export default function Banner() {
+    return (
+        <img id="banner" src={banner} alt="banner inicial" />
+    )
+}
--- frontend/src/screens/Home/Home.jsx
+++ frontend/src/screens/Home/Home.jsx
@@ -1,7 +1,11 @@
 import Header from "../../components/Header/Header";
+import Banner from "../../components/Images/Banner/Banner";
 
-export default function Home(){
+export default function Home() {
     return (
-        <Header />
+        <>
+            <Header />
+            <Banner />
+        </>
     )
 }
