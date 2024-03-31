b760e6dedef6c5a87222f445e503e492fedcc1c7
Gleison <gleisonrgomes@gmail.com>
2024-03-31 13:23:53 -0300

    feat: remove manual cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,22 +6,10 @@ import cors from "cors"
 
 dotenv.config();
 
-const corsOptions = {
-    origin: 'http://localhost:5137',
-    optionsSuccessStatus: 200
-}
 const app = express();
 const port = process.env.PORT;
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5137");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    next();
-});
-
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(express.json());
 app.use(routes);
 
