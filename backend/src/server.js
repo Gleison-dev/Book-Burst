9738fb1671953377ca13469a1d63f8f916f05110
Gleison <gleisonrgomes@gmail.com>
2024-03-31 13:21:39 -0300

    feat: added manual cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,6 +13,14 @@ const corsOptions = {
 const app = express();
 const port = process.env.PORT;
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5137");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+});
+
 app.use(cors(corsOptions))
 app.use(express.json());
 app.use(routes);
