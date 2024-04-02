92cf9ba26648b6bcf983fbbd01daeda2a236fd01
Gleison <gleisonrgomes@gmail.com>
2024-04-01 21:40:47 -0300

    feat: styled in cards

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -3,12 +3,14 @@ import "./style.css"
 export default function Card({ index, title, genre, writer, tags, link_book }) {
     return (
         <>
-            <div key={index} className="div-card">
-                <h2 id="title">Título: {title}</h2>
-                <h3 id="genre">Genêro: {genre}</h3>
-                <h3 id="writer">Escritor: {writer}</h3>
-                <p id="tags">Tags: {tags}</p>
-                <a href={link_book}><button id="btn-link">Link do Livro</button></a>
+            <div className="card">
+                <div key={index} className="div-card">
+                    <h2 id="title">Título: {title}</h2>
+                    <h3 id="genre">Genêro: {genre}</h3>
+                    <h3 id="writer">Escritor: {writer}</h3>
+                    <p id="tags">Tags: {tags}</p>
+                    <a href={link_book}><button id="btn-link">Link do Livro</button></a>
+                </div>
             </div>
         </>
     )
