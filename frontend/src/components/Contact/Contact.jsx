2ea8096e0cd84be6d9b31d758bf93fe138fa6d9d
Gleison <gleisonrgomes@gmail.com>
2024-04-02 23:48:09 -0300

    feat: contacts finished

--- frontend/src/components/Contact/Contact.jsx
+++ frontend/src/components/Contact/Contact.jsx
@@ -8,16 +8,14 @@ export default function Contact() {
             <div className="contact">
                 <div className="div-contact">
                     <div className="title">
-                        <h1 id="title-contact">Contatos:</h1>
+                        <h2 id="title-contact">Contatos:</h2>
                     </div>
                     <div className="contacts">
-                        <div className="first-contact">
-                            <img src={iconLinkedin} />
-                            <a href="https://www.linkedin.com/in/gleison-ribeiro-dev/">Clique Aqui</a>
+                        <div className="first-contact">                         
+                            <a href="https://www.linkedin.com/in/gleison-ribeiro-dev/"><img className="img" src={iconLinkedin} /></a>
                         </div>
-                        <div className="second-contact">
-                            <img src={iconGitHub} />
-                            <a href="https://github.com/Gleison-dev">Clique Aqui</a>
+                        <div className="second-contact">  
+                            <a href="https://github.com/Gleison-dev"><img className="img" src={iconGitHub} /></a>
                         </div>
                     </div>
                 </div>
